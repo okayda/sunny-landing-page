44d562f1381fb55f2b17b047bbda8261141b466b
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-26 10:39:36 -0800

    Add typing text with effect at hero section

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,21 @@
+// sticky scroll navigation
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 440);
 });
 
+// toggling for mobile navigation
 document
   .querySelector(".header-burger-btn")
   .addEventListener("click", function () {
     const header = document.querySelector("header");
     header.classList.toggle("open-nav");
   });
+
+// typing text with effect
+const typed = new Typed(".auto-typed", {
+  strings: ["We", "are", "creatives"],
+  tpyedSpeed: 250,
+  backSpeed: 200,
+  loop: true,
+});
