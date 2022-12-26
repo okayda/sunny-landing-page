b879200ea0614dcdbf8716c0e343633fc609d30e
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-26 06:10:27 -0800

    Add header navigation layout at mobile & desktop

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 440);
 });
+
+document
+  .querySelector(".header-burger-btn")
+  .addEventListener("click", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("open-nav");
+  });
